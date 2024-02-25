f4573613e33676f88cb563753d57b634825a578f
Pacheqo <160760211+Pacheqo@users.noreply.github.com>
2024-02-24 23:08:05 -0500

    Add files via upload

---
+++ Operadores/Arithmetic/residuo.cpp
@@ -0,0 +1,12 @@
+/* En este ejercicio se hará uso del operador ( % ). Este operador se utiliza para calcular el residuo de una división
+entre dos números enteros */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 3;
+	cout << x % y;
+	
+	return 0;
+}
---
+++ Operadores/Arithmetic/suma.cpp
@@ -0,0 +1,11 @@
+/* Este ejercicio contiene un operador de suma ( + ) */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 7;
+	cout << x + y;
+	
+	return 0;
+}
