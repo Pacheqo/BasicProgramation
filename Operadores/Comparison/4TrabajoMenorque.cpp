74b8429ff2f5a746ed86e62c035250b4a3121e9a
Pacheqo <160760211+Pacheqo@users.noreply.github.com>
2024-02-24 23:30:55 -0500

    Add files via upload

---
+++ Operadores/Comparison/4TrabajoMenorque.cpp
@@ -0,0 +1,15 @@
+/* En este ejercicio de hará uso del operador de comparación ( < ). Este operador verifica
+si una variable es menor que a otra. Los operadores de comparación actúan como booleanos, es decir,
+puede ser 1 si es verdadero y 0 si es falso */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 7;
+	int y = 3;
+	cout << (x < y); // Se utilizan paréntesis al verificar esta expresión
+	
+// 7 NO es menor que 3, por lo tanto, esto es falso (0)
+	
+	return 0;
+}
---
+++ Operadores/Comparison/6TrabajoMenoroIgualque.cpp
@@ -0,0 +1,15 @@
+/* En este ejercicio de hará uso del operador de comparación ( <= ). Este operador verifica
+si una variable es menor o igual que otra. Los operadores de comparación actúan como booleanos, es decir,
+puede ser 1 si es verdadero y 0 si es falso */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 3;
+	int y = 3;
+	cout << (x <= y); // Se utilizan paréntesis al verificar esta expresión
+	
+// 3 es menor o igual que 3, por lo tanto, esto es verdadero (0)
+	
+	return 0;
+}
