9a028b0b631fac02270b4769a47ffa17d7d679c0
Pacheqo <160760211+Pacheqo@users.noreply.github.com>
2024-03-06 11:33:29 -0500

    Add files via upload

---
+++ Loops/For/2Example.cpp
@@ -0,0 +1,12 @@
+/* Mismo ejercicio del ejemplo anterior, pero definiendo la condición como 10 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	for (int i = 0; i < 10; i++) {
+		cout << i << "\n";
+	}
+	
+	return 0;
+}
