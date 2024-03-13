55722ceda8037790de16c8b6c6905dd85895cb9d
Pacheqo <160760211+Pacheqo@users.noreply.github.com>
2024-03-13 00:24:41 -0500

    Add files via upload

---
+++ Switch/1LoopSinLoop.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 0;
+	
+	if (i == 9) {
+	   cout << "El numero llego a 9";
+    } 
+	
+	else {
+	     i = ++i;
+	     cout << i << endl;
+    }
+	
+	if (i == 9) {
+	   cout << "El numero llego a 9";
+    }
+	else {
+	   i = ++i;
+	   cout << i << endl;
+    }
+    
+	if (i == 9) {
+	   cout << "El numero llego a 9";
+    }
+	else {
+	  i = ++i;
+	  cout << i << endl;
+    }
+    
+	if (i == 9) {
+		cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+	
+	if (i == 9) {
+		cout << "El numero llego a 9";
+	} 
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+	
+	if (i == 9) {
+		cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+	
+	if (i == 9) {
+		cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+	
+	if (i == 9) {
+		cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+	
+	if (i == 9) {
+	cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+	}
+	
+	if (i == 9) {
+	cout << "El numero llego a 9";
+	}
+	else {
+		i = ++i;
+		cout << i << endl;
+	}
+
+  return 0;
+}
